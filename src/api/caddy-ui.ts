2bdb2d05c573af43a34c99e96b7e7c054ac00a85
Quentin McGaw <quentin.mcgaw@gmail.com>
2020-05-20 20:57:59 -0400

    API queries to Caddy API and Caddy UI server

---
+++ src/api/caddy-ui.ts
@@ -0,0 +1,29 @@
+export async function getCaddyfile(address: string): Promise<string> {
+  const response = await fetch(address + "/caddyfile", {
+    method: "GET",
+  });
+  if (!response.ok) {
+    const message = await response.text();
+    throw new Error(`cannot get Caddyfile: ${message} (${response.statusText})`);
+  }
+  if (response.body !== null) {
+    return await response.text();
+  }
+  return "";
+}
+
+export async function setCaddyfile(address: string, content: string): Promise<string> {
+  const response = await fetch(address + "/caddyfile", {
+    method: "PUT",
+    body: new Buffer(content),
+    headers: { "Content-Type": "application/octet-stream" },
+  });
+  if (!response.ok) {
+    const message = await response.text();
+    throw new Error(`cannot set Caddyfile: ${message} (${response.statusText})`);
+  }
+  if (response.body !== null) {
+    return await response.text();
+  }
+  return "";
+}
