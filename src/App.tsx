4176abf6d12f8957371912257bab0988683b5672
Quentin McGaw <quentin.mcgaw@gmail.com>
2020-05-21 13:37:36 -0400

    Error and success snackbars

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import Title from "components/Title";
 import Actions from "components/Actions";
 import Caddyfile from "components/Caddyfile";
 import Palette from "constants/palette";
+import Snackbar from "@material-ui/core/Snackbar";
+import Alert from "@material-ui/lab/Alert";
 import { getCaddyfile as apiGetCaddyfile, setCaddyfile as apiSetCaddyfile } from "api/caddyfile";
 
 const useStyles = makeStyles(theme => ({
@@ -34,16 +36,53 @@ function App(): JSX.Element {
   const apiEndpoint = `${window.location.href}/api`; // Caddy UI API
   const classes = useStyles();
   const [caddyfileContent, setCaddyfileContent] = React.useState("");
+  const [showSuccess, setShowSuccess] = React.useState(false);
+  const [error, setError] = React.useState(null);
+  const clearError = (): void => {
+    setError(null);
+  };
   const getCaddyfile = async (): Promise<void> => {
-    setCaddyfileContent(await apiGetCaddyfile(apiEndpoint));
+    try {
+      const content = await apiGetCaddyfile(apiEndpoint);
+      setCaddyfileContent(content);
+    } catch (e) {
+      const s = e.toString();
+      if (s !== error) {
+        setError(s);
+      }
+      throw e;
+    }
+  };
+  const setCaddyfile = async (): Promise<void> => {
+    try {
+      await apiSetCaddyfile(apiEndpoint, caddyfileContent);
+      if (!showSuccess) {
+        setShowSuccess(true);
+      }
+    } catch (e) {
+      const s = e.toString();
+      if (s !== error) {
+        setError(s);
+      }
+    }
+  };
+  const hydrate = (): void => {
+    try {
+      getCaddyfile();
+    } catch (e) {}
   };
   const refresh = (): void => {
-    getCaddyfile();
+    try {
+      getCaddyfile();
+      if (!showSuccess) {
+        setShowSuccess(true);
+      }
+    } catch (e) {}
   };
   const upload = (): void => {
-    apiSetCaddyfile(apiEndpoint, caddyfileContent);
+    setCaddyfile();
   };
-  React.useEffect(refresh);
+  React.useEffect(hydrate);
   return (
     <div className={classes.root}>
       <div className={classes.background} />
@@ -52,6 +91,12 @@ function App(): JSX.Element {
         <Actions handleRefresh={refresh} handleUpload={upload} />
         <Caddyfile content={caddyfileContent} onChange={(content: string): void => setCaddyfileContent(content)} />
       </div>
+      <Snackbar open={showSuccess} autoHideDuration={2000} onClose={(): void => setShowSuccess(false)}>
+        <Alert severity="success">Success</Alert>
+      </Snackbar>
+      <Snackbar open={error !== null} autoHideDuration={2000} onClose={clearError}>
+        <Alert severity="error">{error}</Alert>
+      </Snackbar>
     </div>
   );
 }
